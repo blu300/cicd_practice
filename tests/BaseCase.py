680d019fb86eb209e66bb3f10fb43fbec873cbf4
blu300 <bbluestone@outlook.com>
2024-08-09 16:06:49 +0100

    added pytest to yaml

--- tests/BaseCase.py
+++ tests/BaseCase.py
@@ -12,14 +12,9 @@ from src.app import test_app
 class BaseCase(unittest.TestCase):
     def setUp(self):
         # this statement will be executed before testing
-        # self.app = test_app.test_client()
-    #    self.db = db.get_db() # get db by example
         print("Running Test:")
     
     def tearDown(self):
         # this statement will be executed after testing
-        # Delete Database collections after the test is complete
-        # for collection in self.db.list_collection_names():
-        #     self.db.drop_collection(collection)
         print('''Test Complete
               ''' )
